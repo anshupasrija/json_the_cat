19316de55371d2e3b6211f181253ad5993f7effd
anshu pasrija <anshu.pasrija@gmail.com>
2021-08-26 23:06:30 +0000

    index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,27 @@
-const args = process.argv.slice(2);
-const request = require("request");
 
+const request = require("request");
 const fileName = "https://api.thecatapi.com/v1/breeds/search?q=";
-request(fileName + args[0], (error, response, body) => {
-  const data = JSON.parse(body);
-  console.log(data[0]);
-  if (error !== null) {
-    console.log("error");
-    return;
-  }
-  if (args[0] !== data.name) {
-    console.log("Breed not found");
-  }
-});
+
+const fetchBreedDescription = function (breedName, callback) {
+  request(fileName + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (breedName !== data[0].name) {
+      callback("Bread not found", null);
+    }
+    if (response.statusCode !== 200) {
+      callback("Server responded with:" + response.statusCode, null);
+      return;
+    }
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
+
+
